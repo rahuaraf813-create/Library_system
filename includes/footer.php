89b02c10f5bd0ca493f3f4624af42795f01c4639
Arafath Rahman <rahuaraf813@gmail.com>
2026-05-08 14:37:47 +0530

    added footer.php file's codes

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+</div> <footer class="footer mt-auto py-3 bg-light border-top">
+        <div class="container text-center">
+            <span class="text-muted small">
+                Library Management System &copy; <?php echo date('Y'); ?> 
+                | Group Project | Designed by Leader
+            </span>
+        </div>
+    </footer>
+
+    <script src="../assets/js/bootstrap.bundle.min.js"></script>
+    
+    <script src="../assets/js/script.js"></script>
+</body>
+</html>
