64a0d2bcba4a0af9349bd2e8d3b521057c92120c
Arafath Rahman <rahuaraf813@gmail.com>
2026-05-08 13:38:38 +0530

    edited -dp.php and session.php

--- config/db.php
+++ config/db.php
@@ -2,12 +2,10 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "library_db"; 
+$dbname = "library_system"; // Must match your phpMyAdmin database name
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
